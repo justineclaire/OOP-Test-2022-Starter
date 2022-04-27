923b2895caf8001788baaaf42ca35b1265851bd3
justineclaire <langridge.justine@gmail.com>
2022-04-27 17:24:18 +0100

    nematode outline

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -28,9 +28,7 @@ public class NematodeVisualiser extends PApplet
 			{
 				counter = 0;
 			}
-			
 		}		
-
 	}
 
 
@@ -64,16 +62,49 @@ public class NematodeVisualiser extends PApplet
 	{	
 		
 		background(0);
-		
-
+		//initialising colours and which nematode to draw
+		translate(width/2, height/2);
 		float col = map(counter, 0, nems.size(), 0, 255);
 		fill(col, 255, 255);
 		stroke(col, 255, 255);
 		Nematode nem = nems.get(counter);
+
+		//names
 		textSize(30);
-		text(nem.name, width/2, height/5);
+		//text(nem.name, 0, -200);
 
+		//arrows
+		strokeWeight(10);
+		line(-350, 0, -250, 0);
+		line(-350, 0, -300, 20);
+		line(-350, 0, -300, -20);
 
+		line(350, 0, 250, 0);
+		line(350, 0, 300, 20);
+		line(350, 0, 300, -20);
 
+		int circSize = 50;
+
+		int ellipsex = 0;
+		int ellipsey = -50;
+		textAlign(CENTER);
+		text(nem.name, ellipsex, ellipsey - 100);
+		for(int i = 0; i < nem.length; i++)
+		{
+			
+			strokeWeight(4);
+			noFill();
+			ellipse(ellipsex, ellipsey, circSize, circSize);
+
+			if(nem.limbs == true)
+			{
+				line(ellipsex-circSize, ellipsey, ellipsex - circSize/2, ellipsey);
+				line(ellipsex+circSize, ellipsey, ellipsex + circSize/2, ellipsey);
+			}
+
+			ellipsey += 50;
+		}
+
+		
 	}
 }
